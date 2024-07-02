22c6f2cbcf3c581a239ebd907c6fe15ee17490df
abdulrehman0991 <abdul.rehman@invozone.dev>
2024-07-03 03:12:30 +0500

    Added network switch code block

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React,{ useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import Web3 from "web3";
+import walletNetwork from './utils/helpers/walletNetwork';
 
 function App() {
   const [isConnected, setIsConnected] = useState(null);
@@ -26,6 +27,47 @@ function App() {
     }
   };
 
+
+
+const switchNetwork = async (networkKey) => {
+  const network = walletNetwork[networkKey];
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: network[0].chainId }],
+      });
+      console.log(`Switched to the ${networkKey} network`);
+    } catch (switchError) {
+      // The network has not been added to MetaMask
+      if (switchError.code === 4902) {
+        checkNetwork(networkKey);
+      } else {
+        console.error("Cannot switch to the network", switchError);
+      }
+    }
+  } else {
+    console.error("Ethereum provider is not available");
+  }
+};
+
+const checkNetwork = async (networkKey) => {
+  const network = walletNetwork[networkKey];
+  try {
+    await window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: network,
+    });
+    console.log(`Network ${networkKey} added`);
+  } catch (addError) {
+    console.error("Failed to add the network", addError);
+  }
+};
+
+const handleSwitchNetwork = (networkKey) => {
+  switchNetwork(networkKey);
+};
+
   return (
     <div className="App">
       <header className="App-header">
@@ -33,7 +75,24 @@ function App() {
           {isConnected ? "MetaMask Connected" : "Connect to the metamask"}
         </button>
         <p>{walletAddress && `Wallet Address: ${walletAddress}`}</p>
-        <p>{walletBalance && `Wallet Balance: ${walletBalance} ETH`}</p>
+        <p>{walletBalance && `Wallet Balance: ${walletBalance}`}</p>
+        {isConnected && (
+          <div style={{ display: "flex", columnGap: 50 }}>
+            <button
+              className="App-link"
+              onClick={() => handleSwitchNetwork("polygon")}
+            >
+              Switch to the Polygon
+            </button>
+            <button
+              className="App-link"
+              // eslint-disable-next-line no-undef
+              onClick={() => handleSwitchNetwork("sepolia")}
+            >
+              Switch to the Ethereum
+            </button>
+          </div>
+        )}
       </header>
     </div>
   );
